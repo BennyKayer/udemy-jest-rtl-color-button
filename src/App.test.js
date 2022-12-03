f3df0dc57d4ffe49fe210477766a947a64701ebf
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-12-03 16:52:53 +0100

    #02.14 Start Color Button App & logRoles

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { logRoles } from "@testing-library/dom";
 
-test('renders learn react link', () => {
-  // Create virtual dom for argument JSX and expose it through screen global
-  // rest is self explanatory
-  render(<App />);
-  const linkElement = screen.getByText(/learn/i);
-  // assertion
-  expect(linkElement).toBeInTheDocument();
+test("button has correct initial color", () => {
+    const { container } = render(<App />);
+    logRoles(container);
+    const button = screen.getByRole("button", { name: "Change to blue" });
+    expect(button).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button turns blue when clicked", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Change to blue" });
 });
