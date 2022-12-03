98f06d5bd3faa618f17b5d2f4f8a5eccbda5aaaa
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-12-03 10:14:20 +0100

    #01.05 Jest Watch Mode

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // Create virtual dom for argument JSX and expose it through screen global
+  // rest is self explanatory
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn/i);
+  // assertion
   expect(linkElement).toBeInTheDocument();
 });
